cd569ce40bd64bf8117d299726937eb070a96a19
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-09 18:12:51 +0000

    Deploy vvkk77/approver-dashboard to github.com/vvkk77/approver-dashboard.git:gh-pages

---
+++ precache-manifest.71158751efc6205915bea4264c32705d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce5c201828b6e12402e3",
+    "url": "/approver-dashboard/css/app.31da505e.css"
+  },
+  {
+    "revision": "ad115dd1a3a1b56dbcb9bb08c4a779d2",
+    "url": "/approver-dashboard/img/empty-table.ad115dd1.png"
+  },
+  {
+    "revision": "423d5a42de0d7207f7d70a052f86593c",
+    "url": "/approver-dashboard/img/login-bg.423d5a42.png"
+  },
+  {
+    "revision": "27f0a14f6163fb6d5d54a8c71b192f7c",
+    "url": "/approver-dashboard/index.html"
+  },
+  {
+    "revision": "ce5c201828b6e12402e3",
+    "url": "/approver-dashboard/js/app.9cbb9c6d.js"
+  },
+  {
+    "revision": "45554bb535df681a0efd",
+    "url": "/approver-dashboard/js/chunk-vendors.6db40986.js"
+  },
+  {
+    "revision": "e879279f7c107d9faf44f0333b34acef",
+    "url": "/approver-dashboard/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/approver-dashboard/robots.txt"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,30 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/approver-dashboard/precache-manifest.71158751efc6205915bea4264c32705d.js"
+);
+
+workbox.core.setCacheNameDetails({prefix: "approver-dashboard"});
+
+workbox.core.skipWaiting();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
